cff5cc827b12437a3cc821a4b72f85dbda17878f
Daniel Cornelsen <dani179b@edu.eal.dk>
2018-10-03 09:13:27 +0200

    Making code for adding a Workout Seance
    NOT DONE!

---
+++ Dementors/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+using Dementors.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dementors.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("CreateTrainingSession")]
+        public IActionResult CreateWorkoutSeance(WorkoutSeance
+            workoutSeance)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            
+            _db.
+        }
+    }
+}
--- Dementors/Models/WorkoutSeance.cs
+++ Dementors/Models/WorkoutSeance.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,25 @@ namespace Dementors.Model
 {
     public class WorkoutSeance
     {
+        private string _key;
+
+        public string key
+        {
+            get
+            {
+                
+            }
+            set
+            {
+                key
+            }
+        }
+        
+        [Required]
         public int TimeSpent { get; set; }
         public DateTime TimeDateForWorkout { get; set; }
+        [Required]
         public string WorkoutType { get; set; }
-        public int UserId { get; set; }
+        public long UserId { get; set; }
     }
 }
