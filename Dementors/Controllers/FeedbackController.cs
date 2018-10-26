9d998b942f620e4f9f95082b40c1ae255a957b46
Daniel Cornelsen <dani179b@edu.eal.dk>
2018-10-26 11:54:23 +0200

    ælpåkomkl

--- Dementors/Controllers/FeedbackController.cs
+++ Dementors/Controllers/FeedbackController.cs
@@ -2,14 +2,46 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Dementors.Model;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Dementors.Controllers
 {
     public class FeedbackController : Controller
     {
+        private readonly DatabaseContext _db;
+
+        public FeedbackController(DatabaseContext db)
+        {
+            _db = db;
+        }
+        
+        [Route(" ")]
         public IActionResult Index()
         {
+            return View();
+        }
+        
+        [HttpGet, Route("Create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        
+        [HttpPost, Route("Create")]
+        public IActionResult Create(Feedback feedback)
+        {
+
+            if (!ModelState.IsValid)
+                return View();
+
+            feedback.UserId = 1;
+            feedback.Id = 1;
+            feedback.TimeDate = DateTime.Now;
+
+            _db.Feedback.Add(feedback);
+            _db.SaveChanges(); 
+            
             return View();
         }
     }
