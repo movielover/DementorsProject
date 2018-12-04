6f3b4123bb6126eeb2ea83a43128f4537756b1cc
Quân Peter Tran <peterogdan@hotmail.com>
2018-12-04 12:11:36 +0100

    user and password

--- Dementors/Model/Bruger.cs
+++ Dementors/Model/Bruger.cs
@@ -9,6 +9,8 @@ namespace Dementors.Model
         public int Id { get; set; }
         public int UserId { get; set; }
         public string Name { get; set; }
+		public string Username { get; set; }
+	    public string Password { get; set; }
 
     }
 }
